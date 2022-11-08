b95f089efce6431867e958144347709ccab758fc
mrlim0802 <sjlim0802@gmail.com>
2022-11-09 02:20:48 +0700

    set navigator to login and register page

---
+++ lib/pages/login.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:home_cleaning_service_app/pages/welcome.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String title = capitalize("home hub");
+    String description = capitalize("Let's make awesome changes to your home.");
+    String btnLogin = capitalize("login");
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(title),
+              Text(description),
+              SizedBox(
+                child: Form(
+                    child: SingleChildScrollView(
+                  child: Column(
+                    children: [TextFormField(), TextFormField()],
+                  ),
+                )),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text(btnLogin)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,58 +1,85 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:home_cleaning_service_app/pages/login.dart';
+import 'package:home_cleaning_service_app/pages/register.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String btnSignup = capitalize("continues with your account");
-    String btnLogin = capitalize("create account");
+    String btnSignup = capitalize("create account");
+    String btnLogin = capitalize("continues with your account");
     String title = capitalize("home hub");
-    String description = "Keep your home clean and pristine by enlisting the help of a professional cleaner as often as you need.";
+    String description =
+        "Keep your home clean and pristine by enlisting the help of a professional cleaner as often as you need.";
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/welcome/banner.png"),
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color(hexColor('#f36b7f')),
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  
-                ),
+      body: Container(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/welcome/banner.png"),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 1.5,
+                    child: Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(hexColor('#f36b7f')),
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: Text(
+                      description,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(hexColor('#7a7a7a')),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Color(hexColor('#f36b7f'))),
+                            onPressed: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return LoginPage();
+                              }));
+                            },
+                            child: Text(btnLogin)),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                                primary: Color(hexColor('#f36b7f'))),
+                            onPressed: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return RegisterPage();
+                              }));
+                            },
+                            child: Text(btnSignup)),
+                      )
+                    ],
+                  ),
+                ],
               ),
-              Text(
-                description,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color(hexColor('#7a7a7a')),
-                  fontSize: 14,
-      
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Color(hexColor('#f36b7f'))),
-                    onPressed: () {},
-                    child: Text(btnSignup)),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                        primary: Color(hexColor('#f36b7f'))),
-                    onPressed: () {},
-                    child: Text(btnLogin)),
-              )
-            ],
+            ),
           ),
         ),
       ),
@@ -60,11 +87,11 @@ class WelcomePage extends StatelessWidget {
   }
 }
 
-String capitalize (String str) {
-    return str.toLowerCase().split(' ').map((word) {
-      String leftText = (word.length > 1) ? word.substring(1, word.length) : '';
-      return word[0].toUpperCase() + leftText;
-    }).join(' ');
+String capitalize(String str) {
+  return str.toLowerCase().split(' ').map((word) {
+    String leftText = (word.length > 1) ? word.substring(1, word.length) : '';
+    return word[0].toUpperCase() + leftText;
+  }).join(' ');
 }
 
 int hexColor(String color) {
