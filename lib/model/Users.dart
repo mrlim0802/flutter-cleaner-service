ebee01697bd8bce67100e15cc20e8249f1ed0ec5
mrlim0802 <sjlim0802@gmail.com>
2022-11-13 01:33:35 +0700

    install firebase auth and set try catch user data

--- lib/model/Users.dart
+++ lib/model/Users.dart
@@ -1,6 +1,5 @@
 class Users{
   String email;
-  String username;
   String password;
-  Users({required this.email,required this.username, required this.password});
+  Users({required this.email, required this.password});
 }
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors, avoid_print
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:home_cleaning_service_app/model/Users.dart';
@@ -13,89 +17,106 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final formKey = GlobalKey<FormState>();
   Users users = Users(
-    username: ' ',
     email: ' ',
     password: ' ',
   );
+
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     String title = capitalize("home hub");
     String description = capitalize("Let's make awesome changes to your home.");
     String btnLogin = capitalize("login");
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(title),
-              Text(description),
-              SizedBox(
-                child: Form(
-                    key: formKey,
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            validator: RequiredValidator(
-                                errorText: "You must enter your name"),
-                            onSaved: (String? username) {
-                              users.username = username!;
-                            },
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Enter your username',
+    return FutureBuilder(
+      future: firebase,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            appBar: AppBar(title: Text("Error internet")),
+            body: Center(
+              child: Text("${snapshot.error}"),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(title),
+                    Text(description),
+                    SizedBox(
+                      child: Form(
+                          key: formKey,
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                
+                                TextFormField(
+                                  keyboardType: TextInputType.emailAddress,
+                                  validator: MultiValidator([
+                                    EmailValidator(
+                                        errorText: "Invalid Email Format"),
+                                    RequiredValidator(
+                                        errorText: "You must enter your email")
+                                  ]),
+                                  onSaved: (String? email) {
+                                    users.email = email!;
+                                  },
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Enter your email',
+                                  ),
+                                ),
+                                TextFormField(
+                                  obscureText: true,
+                                  validator: RequiredValidator(
+                                      errorText:
+                                          "You must enter your password"),
+                                  onSaved: (String? password) {
+                                    users.password = password!;
+                                  },
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Enter your password',
+                                  ),
+                                )
+                              ],
                             ),
-                          ),
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            validator: MultiValidator([
-                              EmailValidator(errorText: "Invalid Email Format"),
-                              RequiredValidator(
-                                  errorText: "You must enter your email")
-                            ]),
-                            onSaved: (String? email) {
-                              users.email = email!;
+                          )),
+                    ),
+                    SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              if (formKey.currentState!.validate()) {
+                                formKey.currentState?.save();
+                                try {
+                                  await FirebaseAuth.instance
+                                      .createUserWithEmailAndPassword(
+                                          email: users.email,
+                                          password: users.password);
+                                  formKey.currentState?.reset();
+                                } on FirebaseAuthException catch (e) {
+                                  print(e.message);
+                                }
+                              }
                             },
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Enter your email',
-                            ),
-                          ),
-                          TextFormField(
-                            obscureText: true,
-                            validator: RequiredValidator(
-                                errorText: "You must enter your password"),
-                            onSaved: (String? password) {
-                              users.password = password!;
-                            },
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Enter your password',
-                            ),
-                          )
-                        ],
-                      ),
-                    )),
+                            child: Text(btnLogin))),
+                  ],
+                ),
               ),
-              SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState?.save();
-                          print(
-                              "username = ${users.username} email = ${users.email} password = ${users.password}");
-                          formKey.currentState?.reset();
-                        }
-                      },
-                      child: Text(btnLogin))),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        }
+        return Scaffold(
+          body: Center(child: CircularProgressIndicator()),
+        );
+      },
     );
   }
 }
