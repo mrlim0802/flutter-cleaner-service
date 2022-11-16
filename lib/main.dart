228984f0ed667de08012d760fe34beac09161b7c
mrlim0802 <sjlim0802@gmail.com>
2022-11-17 00:45:32 +0700

    edit navidationbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:home_cleaning_service_app/pages/SplashScreen.dart';
-import 'package:home_cleaning_service_app/pages/welcome.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/Reserve.dart
+++ lib/pages/Reserve.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:home_cleaning_service_app/ColorScheme.dart';
+import 'package:home_cleaning_service_app/data/reserveData.dart';
 import 'package:sizer/sizer.dart';
 
 class ReservePage extends StatefulWidget {
@@ -14,12 +15,129 @@ class ReservePage extends StatefulWidget {
 class _ReservePageState extends State<ReservePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      child: Text(
-        'Reserve',
-        style: TextStyle(fontSize: 40.sp),
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+              SizedBox(
+                child: Form(
+                    // key: formKey,
+                    child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // email form
+                      TextFormField(
+                        // keyboardType: TextInputType.emailAddress,
+                        // validator: MultiValidator([
+                        //   EmailValidator(
+                        //       errorText: RegisterData.emailFormatError),
+                        //   RequiredValidator(
+                        //       errorText:
+                        //           RegisterData.emailCheckBlank)
+                        // ]),
+                        // onSaved: (String? email) {
+                        //   users.email = email!;
+                        // },
+                        style: TextStyle(color: greyPrimary),
+                        decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.transparent),
+                                borderRadius: BorderRadius.all(Radius.circular(30))),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.transparent),
+                                borderRadius: BorderRadius.all(Radius.circular(30))),
+                            hintText: 'Enter your email',
+                            prefixIcon: Icon(
+                              Icons.alternate_email_outlined,
+                              color: Color.fromRGBO(123, 123, 123, 1),
+                            ),
+                            fillColor: Color.fromRGBO(192, 192, 192, 0.20),
+                            filled: true),
+                      ),
+                      SizedBox(height: 15.sp),
+
+                      // password form
+                      TextFormField(
+                        obscureText: true,
+                        // validator: RequiredValidator(
+                        //     errorText:
+                        //         "You must enter your password"),
+                        // onSaved: (String? password) {
+                        //   users.password = password!;
+                        // },
+                        decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.transparent),
+                                borderRadius: BorderRadius.all(Radius.circular(30))),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.transparent),
+                                borderRadius: BorderRadius.all(Radius.circular(30))),
+                            hintText: 'Password',
+                            prefixIcon: Icon(
+                              Icons.business_center_outlined,
+                              color: Color.fromRGBO(123, 123, 123, 1),
+                            ),
+                            fillColor: Color.fromRGBO(192, 192, 192, 0.20),
+                            filled: true),
+                      ),
+
+                      SizedBox(height: 15.sp),
+
+                      // Confirm password
+                      TextFormField(
+                        obscureText: true,
+                        // validator: RequiredValidator(
+                        //     errorText:
+                        //         "You must enter your password again."),
+                        // onSaved: (String? confirmPassword) {
+                        //   users.confirmPassword = confirmPassword!;
+                        // },
+                        decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.transparent),
+                                borderRadius: BorderRadius.all(Radius.circular(30))),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.transparent),
+                                borderRadius: BorderRadius.all(Radius.circular(30))),
+                            hintText: 'Confirm Password',
+                            prefixIcon: Icon(
+                              Icons.business_center_outlined,
+                              color: Color.fromRGBO(123, 123, 123, 1),
+                            ),
+                            fillColor: Color.fromRGBO(192, 192, 192, 0.20),
+                            filled: true),
+                      )
+                    ],
+                  ),
+                )),
+              ),
+
+              SizedBox(height: 5.h),
+
+              // login button
+              SizedBox(
+                  width: 50.w,
+                  height: 7.h,
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40))),
+                          backgroundColor: MaterialStateProperty.all<Color>(primary)),
+                      onPressed: () {},
+                      child: Text(ReserveData.btnSignup))),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
